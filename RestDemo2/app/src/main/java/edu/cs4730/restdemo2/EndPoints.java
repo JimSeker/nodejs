c15cba7ea0cf1795356e22a48c3bedee20482c73
Jim Ward <JimSeker@users.noreply.github.com>
2025-02-11 15:45:41 -0700

    added android project for lecture 8.

---
+++ RestDemo2/app/src/main/java/edu/cs4730/restdemo2/EndPoints.java
@@ -0,0 +1,14 @@
+package edu.cs4730.restdemo2;
+
+/**
+ * Created by Seker on 2/11/2025.
+ *
+ * These are just constants, stored here, so if the backend server changes it's quick and easy to fix
+ * them all here, instead of through out the code.
+ *./get
+ */
+
+public class EndPoints {
+    public static final String URL_GET_SCORES = "http://wardpi.eecs.uwyo.edu:3000/api/scores";
+    public static final String URL_REST_SCORES = "http://wardpi.eecs.uwyo.edu:3000/api/scores/";
+}
