9fead5bab80a68eb2bd3be853b3270d5685cbbec
Jim Ward <seker@uwyo.edu>
2025-04-28 14:57:52 -0600

    updated to ES 6 modules

--- lecture5/lib/handlers.js
+++ lecture5/lib/handlers.js
@@ -1,31 +1,33 @@
-exports.api = {}
 
-exports.home = (req, res) => res.render('home')
+export var api = {}
+
+export const home = (req, res) => res.render('home')
 
 // **** these handlers are for browser-submitted forms
-exports.newsletterSignup = (req, res) => {
+export const newsletterSignup = (req, res) => {
   // we will learn about CSRF later...for now, we just
   // provide a dummy value
   res.render('newsletter-signup', { csrf: 'CSRF token goes here' })
 }
-exports.newsletterSignupProcess = (req, res) => {
+export const newsletterSignupProcess = (req, res) => {
   console.log('CSRF token (from hidden form field): ' + req.body._csrf)
   console.log('Name (from visible form field): ' + req.body.name)
   console.log('Email (from visible form field): ' + req.body.email)
   res.redirect(303, '/newsletter-signup/thank-you')
 }
-exports.newsletterSignupThankYou = (req, res) => res.render('newsletter-signup-thank-you')
+export const newsletterSignupThankYou = (req, res) => res.render('newsletter-signup-thank-you')
 // **** end browser-submitted form handlers
 
 
 
 // **** these handlers are for fetch/JSON form handlers
-exports.newsletter = (req, res) => {
+export const newsletter = (req, res) => {
   // we will learn about CSRF later...for now, we just
   // provide a dummy value
   res.render('newsletter', { csrf: 'CSRF token goes here' })
 }
-exports.api.newsletterSignup = (req, res) => {
+
+api.newsletterSignup = (req, res) => {
   console.log('CSRF token (from hidden form field): ' + req.body._csrf)
   console.log('Name (from visible form field): ' + req.body.name)
   console.log('Email (from visible form field): ' + req.body.email)
@@ -33,10 +35,12 @@ exports.api.newsletterSignup = (req, res) => {
 }
 // **** end fetch/JSON form handlers
 
-exports.notFound = (req, res) => res.render('404')
+export const notFound = (req, res) => res.render('404')
 
 // Express recognizes the error handler by way of its four
 // argumetns, so we have to disable ESLint's no-unused-vars rule
 /* eslint-disable no-unused-vars */
-exports.serverError = (err, req, res, next) => res.render('500')
+export const serverError = (err, req, res, next) => res.render('500')
 /* eslint-enable no-unused-vars */
+
+export default { api, home, newsletterSignup, newsletterSignupProcess, newsletterSignupThankYou, newsletter, notFound, serverError }
