df59a013a3f317e9edfade200625d4af9ae6202a
Jim Ward <seker@uwyo.edu>
2026-02-12 13:35:27 -0700

    minor change to example

--- react2/form-app/src/Example3.jsx
+++ react2/form-app/src/Example3.jsx
@@ -2,84 +2,84 @@ import { useState, useReducer } from 'react';
 
 // This is an advanced example of a task management form using useReducer.
 // It allows users to add, edit, mark as done, and delete tasks.
-// It's borrow completely from the official React docs:
+// It's borrowed completely from the official React docs:
 // https://react.dev/learn/managing-state-with-reducers
 
 function Example3() {
-const [tasks, dispatch] = useReducer(
-    tasksReducer,
-    initialTasks
-  );
+    const [tasks, dispatch] = useReducer(
+        tasksReducer,
+        initialTasks
+    );
 
-  function handleAddTask(text) {
-    dispatch({
-      type: 'added',
-      id: nextId++,
-      text: text,
-    });
-  }
+    function handleAddTask(text) {
+        dispatch({
+            type: 'added',
+            id: nextId++,
+            text: text,
+        });
+    }
 
-  function handleChangeTask(task) {
-    dispatch({
-      type: 'changed',
-      task: task
-    });
-  }
+    function handleChangeTask(task) {
+        dispatch({
+            type: 'changed',
+            task: task
+        });
+    }
 
-  function handleDeleteTask(taskId) {
-    dispatch({
-      type: 'deleted',
-      id: taskId
-    });
-  }
+    function handleDeleteTask(taskId) {
+        dispatch({
+            type: 'deleted',
+            id: taskId
+        });
+    }
 
-  return (
-    <>
-      <h3>Example 3, complex forms and useReducer</h3>
-      <AddTask
-        onAddTask={handleAddTask}
-      />
-      <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
-      />
-    </>
-  );
+    return (
+        <>
+            <h3>Example 3, complex forms and useReducer</h3>
+            <AddTask
+                onAddTask={handleAddTask}
+            />
+            <TaskList
+                tasks={tasks}
+                onChangeTask={handleChangeTask}
+                onDeleteTask={handleDeleteTask}
+            />
+        </>
+    );
 }
 
 function tasksReducer(tasks, action) {
-  switch (action.type) {
-    case 'added': {
-      return [...tasks, {
-        id: action.id,
-        text: action.text,
-        done: false
-      }];
-    }
-    case 'changed': {
-      return tasks.map(t => {
-        if (t.id === action.task.id) {
-          return action.task;
-        } else {
-          return t;
+    switch (action.type) {
+        case 'added': {
+            return [...tasks, {
+                id: action.id,
+                text: action.text,
+                done: false
+            }];
         }
-      });
+        case 'changed': {
+            return tasks.map(t => {
+                if (t.id === action.task.id) {
+                    return action.task;
+                } else {
+                    return t;
+                }
+            });
+        }
+        case 'deleted': {
+            return tasks.filter(t => t.id !== action.id);
+        }
+        default: {
+            throw Error('Unknown action: ' + action.type);
+        }
     }
-    case 'deleted': {
-      return tasks.filter(t => t.id !== action.id);
-    }
-    default: {
-      throw Error('Unknown action: ' + action.type);
-    }
-  }
 }
 
 let nextId = 3;
 const initialTasks = [
-  { id: 0, text: 'Philosopher’s Path', done: true },
-  { id: 1, text: 'Visit the temple', done: false },
-  { id: 2, text: 'Drink matcha', done: false }
+    { id: 0, text: 'Philosopher’s Path', done: true },
+    { id: 1, text: 'Visit the temple', done: false },
+    { id: 2, text: 'Drink matcha', done: false }
 ];
 
 // Below is a simple task addition form component.
@@ -87,20 +87,20 @@ const initialTasks = [
 // This could be in its own file, but for the sake of the example, it's included here.
 
 function AddTask({ onAddTask }) {
-  const [text, setText] = useState('');
-  return (
-    <>
-      <input
-        placeholder="Add task"
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <button onClick={() => {
-        setText('');
-        onAddTask(text);
-      }}>Add</button>
-    </>
-  )
+    const [text, setText] = useState('');
+    return (
+        <>
+            <input
+                placeholder="Add task"
+                value={text}
+                onChange={e => setText(e.target.value)}
+            />
+            <button onClick={() => {
+                setText('');
+                onAddTask(text);
+            }}>Add</button>
+        </>
+    )
 }
 
 
